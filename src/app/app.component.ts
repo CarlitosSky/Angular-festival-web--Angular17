77ed8870569351e2bec41429ee390861e4a74de6
Carlitos <carlosperwork@gmail.com>
2024-04-26 13:23:17 +0200

    mejoras navBar y comenzamos con el apartado Productos1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { FooterComponent } from './paginas/footer/footer.component';
 import { NavbarComponent } from './paginas/navbar/navbar.component';
 import { BannerComponent } from './paginas/banner/banner.component';
 import { PrincipalComponent } from './paginas/principal/principal.component';
+import { Productos1Component } from './paginas/productos-1/productos-1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FooterComponent,NavbarComponent,BannerComponent,PrincipalComponent],
+  imports: [RouterOutlet,FooterComponent,NavbarComponent,BannerComponent,PrincipalComponent,Productos1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
