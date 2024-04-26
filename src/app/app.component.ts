3f082d182149b611ec4b319cbe85b73c75685352
Esteban Castañer <esteban.c.pardo@gmail.com>
2024-04-26 12:24:50 +0200

    todo bien

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './paginas/footer/footer.component';
 import { NavbarComponent } from './paginas/navbar/navbar.component';
+import { BannerComponent } from './paginas/banner/banner.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FooterComponent,NavbarComponent],
+  imports: [RouterOutlet,FooterComponent,NavbarComponent,BannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
