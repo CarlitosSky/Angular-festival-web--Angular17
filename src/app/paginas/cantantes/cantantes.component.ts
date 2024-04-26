d006b31ea6eac3acc10180f2a611e2da22a54dcd
Carlitos <carlosperwork@gmail.com>
2024-04-26 14:06:06 +0200

    acabe las ventas, cantantes y primera parte 1.0 acabada

---
+++ src/app/paginas/cantantes/cantantes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cantantes',
+  standalone: true,
+  imports: [],
+  templateUrl: './cantantes.component.html',
+  styleUrl: './cantantes.component.css'
+})
+export class CantantesComponent {
+
+}
