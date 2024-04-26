731cf326d4738d1c55aac39e8d142b57911d4765
Carlitos <carlosperwork@gmail.com>
2024-04-26 13:01:05 +0200

    mejore el banner, cambios en el css de los componentes y cambios en el componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './paginas/footer/footer.component';
 import { NavbarComponent } from './paginas/navbar/navbar.component';
 import { BannerComponent } from './paginas/banner/banner.component';
+import { PrincipalComponent } from './paginas/principal/principal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FooterComponent,NavbarComponent,BannerComponent],
+  imports: [RouterOutlet,FooterComponent,NavbarComponent,BannerComponent,PrincipalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
