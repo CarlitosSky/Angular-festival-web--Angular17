10c23dffd2c5ec50d37a776d8f65be1e88f1b7ae
Carlitos <carlosperwork@gmail.com>
2024-04-26 14:06:06 +0200

    acabe las ventas, cantantes y primera parte 1.0 acabada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { BannerComponent } from './paginas/banner/banner.component';
 import { PrincipalComponent } from './paginas/principal/principal.component';
 import { Productos1Component } from './paginas/productos-1/productos-1.component';
 import { EntradasComponent } from './paginas/entradas/entradas.component';
+import { CantantesComponent } from './paginas/cantantes/cantantes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FooterComponent,NavbarComponent,BannerComponent,PrincipalComponent,Productos1Component,EntradasComponent],
+  imports: [RouterOutlet,FooterComponent,NavbarComponent,BannerComponent,PrincipalComponent,Productos1Component,EntradasComponent,CantantesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
