f553bd2a369756564706316e04fca36074902934
Esteban Castañer <esteban.c.pardo@gmail.com>
2024-04-27 13:00:47 +0200

    pagina de carrito acabada

---
+++ src/app/paginas/carrito-form/carrito-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrito-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './carrito-form.component.html',
+  styleUrl: './carrito-form.component.css'
+})
+export class CarritoFormComponent {
+
+}
