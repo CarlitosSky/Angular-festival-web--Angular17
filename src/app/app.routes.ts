0bf8e79ddb3cb5fc52dafc3687640d14efb07436
Carlitos <carlosperwork@gmail.com>
2024-04-27 11:25:24 +0200

    creacion de componente paginaNotFound y creacion de rutas y caminos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,26 @@
 import { Routes } from '@angular/router';
+import { PrincipalComponent } from './paginas/principal/principal.component';
+import { PagProductosComponent } from './paginas/pag-productos/pag-productos.component';
+import { PagContactoComponent } from './paginas/pag-contacto/pag-contacto.component';
+import { PagRegistrarseComponent } from './paginas/pag-registrarse/pag-registrarse.component';
+import { PagInicioSesionComponent } from './paginas/pag-inicio-sesion/pag-inicio-sesion.component';
+import { PagPagoComponent } from './paginas/pag-pago/pag-pago.component';
+import { PagCarritoComponent } from './paginas/pag-carrito/pag-carrito.component';
+import { PageNotFoundComponent } from './paginas/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     //Routes - NO TOCAR
+    {path: "home", title:"Home",component: PrincipalComponent},
+    {path: "productos", title:"Productos",component: PagProductosComponent},
+    {path: "contacto", title:"Contacto",component: PagContactoComponent},
+    {path: "registro", title:"Registrate",component: PagRegistrarseComponent},
+    {path: "inicioSesion", title:"Inicia sesion",component: PagInicioSesionComponent},
+    {path: "pago", title:"Metodo de pago",component: PagPagoComponent},
+    {path: "carrito", title:"Tu carrito",component: PagCarritoComponent},
 
 
-    
-
-
-
-
-
-
-
-
+        //ULTIMAS RUTAS SIEMPRE
+    {path: "", redirectTo: "/home", pathMatch: "full"},
+    {path: "##", component: PageNotFoundComponent}
 
 ];
