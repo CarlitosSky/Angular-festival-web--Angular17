456ce00dc12dc1b3a23e0129a93de445721ea6d9
Carlitos <carlosperwork@gmail.com>
2024-04-27 13:00:47 +0200

    pagina de carrito acabada

--- src/app/paginas/pag-carrito/pag-carrito.component.ts
+++ src/app/paginas/pag-carrito/pag-carrito.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CarritoFormComponent } from '../carrito-form/carrito-form.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-pag-carrito',
   standalone: true,
-  imports: [],
+  imports: [CarritoFormComponent,FooterComponent],
   templateUrl: './pag-carrito.component.html',
   styleUrl: './pag-carrito.component.css'
 })
