a5e1553c60229d5e90038901d708e32713e90c13
Carlitos <carlosperwork@gmail.com>
2024-04-27 15:18:22 +0200

    routes cambiadaas,pagina 404 funcionando y acabada y todos los links acabados,pagina acabada version 1.0

--- src/app/paginas/productos-1/productos-1.component.ts
+++ src/app/paginas/productos-1/productos-1.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-productos-1',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './productos-1.component.html',
   styleUrl: './productos-1.component.css'
 })
