d99e7b10080b3d5b462f7520c9709d7ed1e91853
Esteban Castañer <esteban.c.pardo@gmail.com>
2024-04-27 13:38:57 +0200

    pagina de registrarse acabada

--- src/app/paginas/pag-registrarse/pag-registrarse.component.ts
+++ src/app/paginas/pag-registrarse/pag-registrarse.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+import { RegisFormComponent } from '../regis-form/regis-form.component';
 
 @Component({
   selector: 'app-pag-registrarse',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent,RegisFormComponent],
   templateUrl: './pag-registrarse.component.html',
   styleUrl: './pag-registrarse.component.css'
 })
