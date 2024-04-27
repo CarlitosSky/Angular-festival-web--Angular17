f991c3339b2c7fac1e1687e8d42b3b5dd827d6fd
Carlitos <carlosperwork@gmail.com>
2024-04-27 14:13:35 +0200

    pagina de productos acabada

--- src/app/paginas/pag-productos/pag-productos.component.ts
+++ src/app/paginas/pag-productos/pag-productos.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+import { ProCartasComponent } from '../pro-cartas/pro-cartas.component';
 
 @Component({
   selector: 'app-pag-productos',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent,ProCartasComponent],
   templateUrl: './pag-productos.component.html',
   styleUrl: './pag-productos.component.css'
 })
---
+++ src/app/paginas/pro-cartas/pro-cartas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pro-cartas',
+  standalone: true,
+  imports: [],
+  templateUrl: './pro-cartas.component.html',
+  styleUrl: './pro-cartas.component.css'
+})
+export class ProCartasComponent {
+
+}
