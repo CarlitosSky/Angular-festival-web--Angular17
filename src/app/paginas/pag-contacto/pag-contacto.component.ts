0dfdb1d46401eb5589df8f7b9453619e43c21ae5
Carlitos <carlosperwork@gmail.com>
2024-04-27 11:12:06 +0200

    preparacion de importaciones y rutas

--- src/app/paginas/pag-contacto/pag-contacto.component.ts
+++ src/app/paginas/pag-contacto/pag-contacto.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-pag-contacto',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterLinkActive,NavbarComponent,FooterComponent],
   templateUrl: './pag-contacto.component.html',
   styleUrl: './pag-contacto.component.css'
 })
