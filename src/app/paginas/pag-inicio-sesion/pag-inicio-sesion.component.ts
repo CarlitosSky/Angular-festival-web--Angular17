bc812dd28825f5ba0cf4b45be299c80154b38bb9
Emilio <eljishiemilio@gmail.com>
2024-04-27 13:54:21 +0200

    acbaada pagina y enlaces de pagina inicio sesion

--- src/app/paginas/pag-inicio-sesion/pag-inicio-sesion.component.ts
+++ src/app/paginas/pag-inicio-sesion/pag-inicio-sesion.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+import { IniFormComponent } from '../ini-form/ini-form.component';
 
 @Component({
   selector: 'app-pag-inicio-sesion',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent,IniFormComponent],
   templateUrl: './pag-inicio-sesion.component.html',
   styleUrl: './pag-inicio-sesion.component.css'
 })
