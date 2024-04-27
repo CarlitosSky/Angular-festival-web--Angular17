9bf7662ebb0466779b4090e263b6bed931a96045
Carlitos <carlosperwork@gmail.com>
2024-04-27 13:38:57 +0200

    pagina de registrarse acabada

---
+++ src/app/paginas/regis-form/regis-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-regis-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './regis-form.component.html',
+  styleUrl: './regis-form.component.css'
+})
+export class RegisFormComponent {
+
+}
