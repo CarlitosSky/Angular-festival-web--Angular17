ca66acdc69401ce339483103c0f601207251f049
Carlitos <carlosperwork@gmail.com>
2024-04-27 13:54:21 +0200

    acbaada pagina y enlaces de pagina inicio sesion

---
+++ src/app/paginas/ini-form/ini-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ini-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './ini-form.component.html',
+  styleUrl: './ini-form.component.css'
+})
+export class IniFormComponent {
+
+}
